3bbcd8d1acbfc8fab57ab071cc7b2fb6baa3fd9a
tsierens <tsierens@perimeterinstitute.ca>
2016-05-29 20:34:18 -0400

    some changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import requests
 import simplejson
+import os
 from bokeh.embed import components
 from bokeh.plotting import figure
 
@@ -15,4 +16,5 @@ def index():
   return render_template('index.html')
 
 if __name__ == '__main__':
+  port = int(os.environ.get("PORT",5000))
   app.run(debug=True, host='0.0.0.0', port=port)
